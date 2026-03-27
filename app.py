47c704ffaafab30befb16cb3089946b404fe127c
comprasconstructoracrv-afk <comprasconstructoracrv@gmail.com>
2026-03-27 11:56:03 -0500

    login sistema

--- app.py
+++ app.py
@@ -1,10 +1,57 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
+from models import db, Usuario
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///financiera.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+# Crear base de datos
+with app.app_context():
+    db.create_all()
+with app.app_context():
+    if not Usuario.query.filter_by(username='admin').first():
+        nuevo = Usuario(username='admin', password='1234', rol='admin')
+        db.session.add(nuevo)
+        db.session.commit()
 
 @app.route('/')
 def inicio():
-    return "Sistema financiero funcionando 🚀"
+    return redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form['username']
+        password = request.form['password']
+
+        usuario = Usuario.query.filter_by(username=user, password=password).first()
+
+        if usuario:
+            session['user'] = usuario.username
+            session['rol'] = usuario.rol
+            return redirect('/dashboard')
+        else:
+            return "Usuario o contraseña incorrectos"
+
+    return render_template('login.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user' not in session:
+        return redirect('/login')
+
+    return f"Bienvenido {session['user']} - Rol: {session['rol']}"
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
+
 
 if __name__ == "__main__":
     app.run()
---
+++ models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    rol = db.Column(db.String(20), nullable=False)  # admin, cobrador, cliente
